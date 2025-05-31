a9e226eca1831714e66406c5d6f45722721e4aca
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-05-31 11:47:00 -0500

    Create physics_modalContent.js

---
+++ menu/physics_modalContent.js
@@ -0,0 +1,9 @@
+window.physicsResearchContent = {
+  "Working at CERN": `
+    <h3>ZDC Data Acquisition</h3>
+    <h3>CMS Control Center Shifts</h3>
+  `,
+
+  "Quantum Chromodynamics": `
+   <p>In progress</p>
+};
