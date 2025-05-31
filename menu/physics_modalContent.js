84863dcd451f01e53209221f6c16e8c5ea3732ba
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-05-31 14:53:08 -0500

    add modal blurb

--- menu/physics_modalContent.js
+++ menu/physics_modalContent.js
@@ -1,6 +1,7 @@
 window.physicsResearchContent = {
   "Working at CERN": `
     <i>ZDC Data Acquisition</i>
+    <p>While at CERN during Fall 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking.</p>
     <i>CMS Control Center Shifts</i>
   `,
 
