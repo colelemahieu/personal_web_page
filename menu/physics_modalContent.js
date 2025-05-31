6121aa04c2fdc1c302334f22ed55ce0a8f88b142
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-05-31 15:12:44 -0500

    center image

--- menu/physics_modalContent.js
+++ menu/physics_modalContent.js
@@ -2,10 +2,11 @@ window.physicsResearchContent = {
   "Working at CERN": `
     <i>ZDC Data Acquisition</i>
     <p>While at CERN in the Fall of 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking.</p>
-    <figure>
-      <img src="Images/lhc_tunnel.jpg" alt="LHC Tunnel" style="max-width:60%; height:auto;">
-      <figcaption>The ZDC in the LHC tunnel</figcaption>
+    <figure style="text-align: center;">
+      <img src="Images/lhc_tunnel.jpg" alt="LHC Tunnel" style="display: block; margin: auto; max-width: 80%; height: auto;">
+      <figcaption>The Large Hadron Collider tunnel at CERN</figcaption>
     </figure>
+
     <i>CMS Control Center Shifts</i>
   `,
 
