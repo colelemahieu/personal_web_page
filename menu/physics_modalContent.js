5f2a84be88c504c2a1afc10c2c89963a5d09af00
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-05-31 15:01:11 -0500

    add physics modal image

--- menu/physics_modalContent.js
+++ menu/physics_modalContent.js
@@ -2,6 +2,10 @@ window.physicsResearchContent = {
   "Working at CERN": `
     <i>ZDC Data Acquisition</i>
     <p>While at CERN during Fall 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking.</p>
+    <figure>
+      <img src="Images/lhc_tunnel.jpg" alt="LHC Tunnel" style="max-width:60%; height:auto;">
+      <figcaption>The ZDC in the LHC tunnel</figcaption>
+    </figure>
     <i>CMS Control Center Shifts</i>
   `,
 
