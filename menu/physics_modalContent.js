72e7a312aab9d6da79b0767cbb213ce3fda4951b
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-06-01 15:06:38 -0500

    working on zdc modal content

--- menu/physics_modalContent.js
+++ menu/physics_modalContent.js
@@ -4,11 +4,11 @@ window.physicsResearchContent = {
     <p>While at CERN in the Fall of 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking. 
     There are two ZDCs, each located 140 meters down the beamline from either side of the CMS interaction point. Their 
     purpose is to detect energy deposits from neutral particles (photons and neutrons) resulting from heavy ion collisions. 
-    Because of their neutral charge, these particles continue traveling down the beamline, unaffected by CMS magents, until 
-    they reach the ZDCs.</p>
-    <p>For my first task at CERN, I tested the detector's performance while above ground. Once that was complete, the detector 
-    was lowered into the tunnel and I assisted with installation. This involved debugging the ZDC's data acquisition system 
-    to ensure </p>
+    Because of their neutral charge, these particles are unaffected by CMS magnets and continue traveling down the beamline 
+    until they hit the ZDCs.</p>
+    <p>For my first task at CERN, I tested the performance of one of these ZDCs above ground. After operational tests, the 
+    detector was lowered into the tunnel and I assisted with installation. This involved debugging the ZDC's data acquisition system 
+    to ensure successful transfer of data from the detector to above-ground electronics.</p>
     <figure style="text-align: center;">
       <img src="Images/lhc_tunnel.jpg" alt="LHC Tunnel" style="display: block; margin: auto; max-width: 60%; height: auto;">
       <figcaption>The Large Hadron Collider tunnel at CERN</figcaption>
