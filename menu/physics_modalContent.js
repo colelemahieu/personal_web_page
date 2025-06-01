32a34d297151608cf617572e29bc4e5ee4f7e142
colelemahieu <55812889+colelemahieu@users.noreply.github.com>
2025-06-01 14:31:53 -0500

    zdc description

--- menu/physics_modalContent.js
+++ menu/physics_modalContent.js
@@ -1,7 +1,11 @@
 window.physicsResearchContent = {
   "Working at CERN": `
     <i style="color: #33658A;">ZDC Data Acquisition</i>
-    <p>While at CERN in the Fall of 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking.</p>
+    <p>While at CERN in the Fall of 2022, I helped prepare the CMS Zero Degree Calorimeters (ZDCs) for data taking. 
+    There are two ZDCs, each located 140 meters down the beamline from either side of the CMS interaction point. Their 
+    purpose is to record energy deposits from neutral particles (photons and neutrons) resulting from heavy ion collisions. 
+    Because of their neutral charge, these particles continue traveling down the beamline, unaffected by CMS magents, until 
+    they reach the ZDCs.</p>
     <figure style="text-align: center;">
       <img src="Images/lhc_tunnel.jpg" alt="LHC Tunnel" style="display: block; margin: auto; max-width: 80%; height: auto;">
       <figcaption>The Large Hadron Collider tunnel at CERN</figcaption>
